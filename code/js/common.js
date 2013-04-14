50e72db42dcdad5b235bce0ca8cd7b843de848a3
aska <asya.zaharova@gmail.com>
2013-04-15 02:51:51 +0400

    new pages

--- code/js/common.js
+++ code/js/common.js
@@ -1,26 +1,35 @@
 $(document).ready(function() {
-	$('#slides').slides({
-		preload: true,
-		generateNextPrev: true,
-		generatePagination:false
-	});
-	$( "#tabs" ).tabs();
 
-	if ($.browser.msie && $.browser.version < 9) {
-		$('.articles-list_top .block_article:nth-child(even)').addClass('block_article_even');
-		$('.articles-list_bottom .block_article:nth-child(odd)').addClass('block_article_odd');
+$('.portfolio__item a').attr('rel', 'media-gallery').fancybox({
+	openEffect : 'none',
+	closeEffect : 'none',
+	prevEffect : 'none',
+	nextEffect : 'none',
+	padding:0,
+	margin:0,
+	autoResize:false,
+	fitToView:false,
+	closeBtn:false,
+	arrows : true,
+	scrolling:'no',
+
+	helpers: {
+	    overlay : {
+		locked     : false
+	    }
 	}
+
+    });
+
+
 
 	$('input, textarea').placeholder();
 
-	$('.ico_collapsed').click(function(){
-		$(this).parents('.comment-single').next().slideToggle(500);
-		$(this).toggleClass('ico_expanded');
-	});
-
 	$('.jqtransform').jqTransform();
 
-  
+
+
+
 
 
 });
